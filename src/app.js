2943bb944be587e54cbb14adf8e1bcb18dba8b75
galguss <gal.gus12@gmail.com>
2025-12-17 20:20:13 +0200

    2

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello from CD with GitHub Actions! for image private!🚀");
+  res.send("Hello from CD with GitHub Actions! for image private2!🚀");
 });
 
 app.get("/health", (req, res) => {
